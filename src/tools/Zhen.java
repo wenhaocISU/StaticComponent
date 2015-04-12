b2d8915e08f9e2c5d7e101491ab4a685f64c7a12
Wenhao Chen <xjtujack@gmail.com>
2015-04-12 12:10:25 -0500

    zhen

--- src/tools/Zhen.java
+++ src/tools/Zhen.java
@@ -1,6 +1,8 @@
 package tools;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import staticFamily.StaticApp;
@@ -10,22 +12,27 @@ import staticFamily.StaticMethod;
 public class Zhen {
 
 	/**
-	 * Return all the lines in the app that aren't in 'lines'
+	 * Return all the lines in the app that didn't get hit
+	 * The result format:
+	 * 	Key set: Class name. Format example: com.example.fast
+	 * 	Value set: Missing lines within this class. Format is the same as the input Set<String> hitLines.
 	 * */
-	Set<String> getMissingLines(Set<String> lines, StaticApp staticApp)
+	Map<String, Set<String>> getMissingLines(Set<String> hitLines, StaticApp staticApp)
 	{
-		Set<String> result = new HashSet<String>();
+		Map<String, Set<String>> result = new HashMap<String, Set<String>>();
 		for (StaticClass c : staticApp.getClasses())
 		{
+			Set<String> missingLinesInClass = new HashSet<String>();
 			for (StaticMethod m : c.getMethods())
 			{
 				for (int i : m.getSourceLineNumbers())
 				{
 					String thisLine = c.getJavaName() + ":" + i;
-					if (!lines.contains(thisLine))
-						result.add(thisLine);
+					if (!hitLines.contains(thisLine))
+						missingLinesInClass.add(thisLine);
 				}
 			}
+			result.put(c.getJavaName(), missingLinesInClass);
 		}
 		return result;
 	}
